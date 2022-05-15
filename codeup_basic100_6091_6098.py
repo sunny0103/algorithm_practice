547559ea2861f5f2bc2ee13b90723f0fe7e52f5b
Jungsun Lee <68505122+sunny0103@users.noreply.github.com>
2022-05-15 23:13:35 +0900

    Add files via upload

---
+++ codeup_basic100_6091_6098.py
@@ -0,0 +1,24 @@
+# 6091. 함께 문제 푸는 날
+# a, b, c = map(int, input().split())
+# d = 1
+# while d%a!=0 or d%b!=0 or d%c!=0:
+#     d+=1
+# print(d)
+
+#6092. 이상한 출석 번호 부르기1
+# import numpy as np
+# n = int(input())
+# a = list(map(int,input().split()))
+# d = list(np.zeros(24))
+# for i in range(n):
+#     d[a[i]] +=1
+# for i in range(1,24):
+#     print(int(d[i]), end=' ')
+
+#6093.  이상한 출석 번호 부르기2
+
+#6094.
+#6095.
+#6096.
+#6097.
+#6098.
