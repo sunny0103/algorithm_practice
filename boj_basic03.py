55bc0e6884129c6858b2c275cf7b452bd37091cf
Jungsun Lee <68505122+sunny0103@users.noreply.github.com>
2022-06-27 21:12:38 +0900

    Add files via upload

---
+++ boj_basic03.py
@@ -0,0 +1,162 @@
+# ### 백준 알고리즘 기초
+# # 21.
+# # 11656 점미사 배열
+# import sys
+# strings = sys.stdin.readline().rstrip()
+
+# li = []
+
+# for i in range(len(strings)):
+#     li.append(strings[i:])
+
+# li.sort()
+
+# for l in li:
+#     print(l)
+
+# ## 수학1
+# # 22
+# # 10430 나머지
+# A, B, C = map(int, input().split())
+# print((A+B)%C)
+# print(((A%C) + (B%C))%C)
+# print((A*B)%C)
+# print(((A%C) * (B%C))%C)
+
+# # 23
+# # 2609 최대공약수 최소 공배수
+# '''
+# 최대공약수와 최소공배수의 관계
+# 유클리드 호제법
+# a * b = gcd(a,b) * lcm(a, b)
+# gcd : greatest common divisor
+# ldm : least common multiple
+# '''
+# a, b = map(int, input().split())
+
+# def gcd(a,b):
+#     if a%b!=0:
+#         return gcd(b,a%b)
+#     else:
+#         return b
+
+# print(gcd(a, b))
+# print(int(a*b/gcd(a, b)))
+
+# # 24
+# # 1934 최소공배수
+
+# def gcd(a,b):
+#     if a%b!=0:
+#         return gcd(b,a%b)
+#     else:
+#         return b
+
+# def lcm (a,b):
+#     return a*b/gcd(a,b)
+
+# t = int(input())
+
+# for _ in range(t):
+#     a, b = map(int, input().split())
+#     print(int(lcm(a,b)))
+
+# # 25
+# # 1929 소수 찾기
+# '''
+# 1과 자기 자신을 제외한 약수가 존재하는지 확인하려면, 자신의 루트 값까지 확인하면 된다.
+# 25의 경우 1과 25를 제외한 루트값이 5까지 확인하면 된다.
+# '''
+# def is_prime(n):
+#     if n<2:
+#         return False
+#     for i in range(2,int(n**0.5)+1):
+#         if n%i ==0:
+#             return False
+#     return True
+
+# m, n = map(int, input().split())
+
+# for i in range(m,n+1):
+#     if is_prime(i):
+#         print(i)
+
+# # 26
+# # 1978 소수찾기
+
+# def is_prime(n):
+#     if n<2:
+#         return False
+#     for i in range(2, int(n**0.5)+1):
+#         if n%i == 0:
+#             return False
+#     return True
+
+# n = int(input())
+# li = list(map(int, input().split()))
+# result = 0
+# for j in li:
+#     if is_prime(j):
+#         result+=1
+# print(result)
+    
+# 27 
+# 6588 골드바흐의 추측
+'''
+개념
+1. n까지 주어졌을때 n까지 모든 소수 출력
+2. (n-출력된 소수) 가 소수인지 확인
+3. 오름차순 정렬
+4. 중복제거
+'''
+def is_prime(n):
+    if n<2:
+        return False
+    for i in range(2, int(n**0.5)+1):
+        if n%i == 0:
+            return False
+    return True
+
+# # 28
+# # 10872 팩토리얼
+
+# def factorial(n):
+#     if n<=1:
+#         return 1
+#     return n * factorial(n-1)
+
+# n = int(input())
+
+# s = str(factorial(n))
+# count = 0
+# for i in range(len(s)-1,-1,-1):
+#     if s[i]=='0':
+#         count+=1
+#     else:
+#         break
+# print(count)
+
+# 29
+# 2004 조합0의갯수
+# https://deok2kim.tistory.com/195
+'''
+순열(permutation):
+nPr = n!/(n-r)!
+조합(combination):
+nCr = n!/(n-r)!*r!
+'''
+# 문제는 끝자리가 0의 갯수 이므로 10의 배수 :2 와 5의 지수의 갯수로 구함
+
+def factorial_5_2_count(num, k):
+    count = 0
+    while num:
+        num//=k
+        count+=num
+    return count
+
+n, m = map(int, input().split())
+
+five_count = factorial_5_2_count(n,5) - factorial_5_2_count(m,5) - factorial_5_2_count((n-m),5)
+two_count = factorial_5_2_count(n,2) - factorial_5_2_count(m,2) - factorial_5_2_count((n-m),2)
+# 5와 2 의 지수 중 작은 값
+print(min(five_count, two_count))
